503cbbc0843b031916f044c4d437348c2db7968d
tamir.rustamov <timorust1@gmail.com>
2024-07-21 23:48:52 +0300

    user model

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,7 +1,35 @@
+import { Heart } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function ProductCard({ product }: { product: ProductType }) {
-	return <div>ProductCard</div>
+	return (
+		<Link
+			href={`/products/${product._id}`}
+			className='w-[220px] flex flex-col gap-2'
+		>
+			<Image
+				src={product.media[0]}
+				alt='product'
+				width={250}
+				height={300}
+				className='h-[250px] rounded-lg object-cover'
+			/>
+
+			<div>
+				<p className='text-base-bold'>{product.title}</p>
+				<p className='text-small-medium text-grey-2'>{product.category}</p>
+			</div>
+
+			<div className='flex justify-between items-center'>
+				<p>${product.price}</p>
+				<button>
+					<Heart />
+				</button>
+			</div>
+		</Link>
+	)
 }
 
 export default ProductCard
---
+++ lib/models/User.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+	clerkId: String,
+	wishlist: Array,
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+})
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+
+export default User
