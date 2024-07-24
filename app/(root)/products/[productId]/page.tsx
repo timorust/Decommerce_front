cb55fae10f996a6154de0b37a1a9829fe1f66ac1
tamir.rustamov <timorust1@gmail.com>
2024-07-24 16:28:35 +0300

    product details

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,7 +1,20 @@
+import Gallery from '@/app/components/Gallery'
+import { getProductDetails } from '@/lib/actions'
 import React from 'react'
 
-const ProductDetails = () => {
-	return <div>ProductDetails</div>
+const ProductDetails = async ({
+	params,
+}: {
+	params: { productId: string }
+}) => {
+	const productDetails = await getProductDetails(params.productId)
+	console.log(productDetails)
+
+	return (
+		<div className='flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center'>
+			<Gallery productMedia={productDetails.media} />
+		</div>
+	)
 }
 
 export default ProductDetails
